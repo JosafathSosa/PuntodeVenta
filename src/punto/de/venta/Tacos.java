380a3b391ba00a534e732e6d53cfc7867297d440
Josa <rsp_63@hotmail.com>
2022-03-05 22:26:43 -0600

    Primer commit

---
+++ src/punto/de/venta/Tacos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package punto.de.venta;
+
+/**
+ *
+ * @author josafathsosajimenez
+ */
+public class Tacos {
+    
+    int precio ;
+    
+    public Tacos(){
+        precio = 12;
+    }
+    
+    
+    
+    
+}
---
+++ src/punto/de/venta/Torta.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package punto.de.venta;
+
+/**
+ *
+ * @author josafathsosajimenez
+ */
+public class Torta {
+    
+    int precio;
+    
+    public Torta(){
+        precio = 40;
+    }
+    
+}
